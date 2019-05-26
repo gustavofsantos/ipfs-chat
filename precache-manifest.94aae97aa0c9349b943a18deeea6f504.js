82262183ab397b51aaa3fb52b54c184cea38c7ae
Gustavo Santos <gustavofsantos@outlook.com>
2019-05-25 22:20:53 -0300

    Updates

---
+++ precache-manifest.94aae97aa0c9349b943a18deeea6f504.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4b01dd3db8c9675ba4fb3b6e2e3fbc",
+    "url": "/ipfs-chat/index.html"
+  },
+  {
+    "revision": "4645206a73135a3fe870",
+    "url": "/ipfs-chat/static/css/main.06834b8e.chunk.css"
+  },
+  {
+    "revision": "1508e5b6f7a5007ca3bb",
+    "url": "/ipfs-chat/static/js/2.197b69a4.chunk.js"
+  },
+  {
+    "revision": "4645206a73135a3fe870",
+    "url": "/ipfs-chat/static/js/main.99344612.chunk.js"
+  },
+  {
+    "revision": "d020640af89f471d820e",
+    "url": "/ipfs-chat/static/js/runtime~main.3c27f136.js"
+  }
+]);
